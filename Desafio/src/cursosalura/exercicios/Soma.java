c2ef2bf2b8146a7a84ab46a28be5bd56454f2677
Eduardo Trandafilov <eduardo.trandafilov@gmail.com>
2025-04-03 17:05:07 -0300

    criando a primeira aplicacao

---
+++ Desafio/src/cursosalura/exercicios/Soma.java
@@ -0,0 +1,13 @@
+package cursosalura.exercicios;
+
+public class Soma {
+	public static void main(String[] args) {
+		 int num1 = 10;
+		 int num2 = 5;
+		 int soma = 0;
+		 
+		 soma = num1 + num2;
+		 
+		 System.out.println(soma);
+	}
+}
---
+++ Projeto1/src/cursoalura/exercicios/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+package cursoalura.exercicios;
+
+public class PrimeiraClasse {
+	public static void main(String[] args) {
+		System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+	}
+}
