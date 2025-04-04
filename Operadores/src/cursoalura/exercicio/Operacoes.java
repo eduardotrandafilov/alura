6add023ec7c1d77fd16cdbe26627b7a9935d4aad
Eduardo Trandafilov <eduardo.trandafilov@gmail.com>
2025-04-03 23:04:28 -0300

    operacoes em java

---
+++ Operadores/src/cursoalura/exercicio/Operacoes.java
@@ -0,0 +1,16 @@
+package cursoalura.exercicio;
+
+public class Operacoes {
+	public static void main(String[] args) {
+		/*
+		 * int num = 5; int resultado = ++num; //num é incrementado para 6 e depois
+		 * atribuído a resultado System.out.println(num); // imprime 6
+		 * System.out.println(resultado); // imprime 6
+		 */		
+		/*int num = 5;
+		int resultado = num++; //num é atribuído primeiramente à variável resultado e depois incrementado para 6
+		System.out.println(num); // imprime 6
+		System.out.println(resultado); // imprime 5
+		*/
+	}
+}
